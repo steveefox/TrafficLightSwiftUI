846584ac3022a9d694d186f5b89b9b29992089eb
Nikita Begletskiy <steveefox50@icloud.com>
2020-08-08 11:45:25 +0300

    Initial Commit

---
+++ TrafficLightSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Nikita on 8/8/20.
+//  Copyright © 2020 Nikita Begletskiy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
